bd3523a4f93493b83af30d7a93a648f474da4961
brendahuwitonze <uwitonzebrendah@gmail.com>
2020-12-21 02:35:55 +0200

    added another layout

--- app/src/main/java/com/moringaschool/blibrary/MainActivity.java
+++ app/src/main/java/com/moringaschool/blibrary/MainActivity.java
@@ -2,13 +2,40 @@ package com.moringaschool.blibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+    EditText username,password;
+    TextView tx1;
+    int counter = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button)findViewById(R.id.button);
+        username = (EditText)findViewById(R.id.editText);
+        password= (EditText)findViewById(R.id.editText2);
+        tx1 = (TextView)findViewById(R.id.textview);
+        tx1.setVisibility(View.GONE);
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
+
---
+++ app/src/main/java/com/moringaschool/blibrary/booksAvailable.java
@@ -0,0 +1,14 @@
+package com.moringaschool.blibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class booksAvailable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_books_available);
+    }
+}
